924bc99c69111ac57a0aca2813911c079c9816a8
SaintofBlood <kochanowicz.p.m@gmail.com>
2022-09-30 10:59:39 +0800

    Update Printing.java

--- Project1/src/Printing.java
+++ Project1/src/Printing.java
@@ -7,19 +7,25 @@ public class Printing {
 	
 	public static void PrintDatabaseInfo(MainMemory _Mm) {
 		System.out.println("################ DATABASE INFO ################");
-		System.out.println("Memory Size: " + _Mm.memPoolSize + " bytes");
+		System.out.println("Memory Size: " + _Mm.memPoolSize + " bytes");\
+		System.out.println("The Size of database: " + _Mm.sumRecSize + (_Mm.ind_N * _Mm.block_size));
 		System.out.println("Block Size: " + _Mm.block_size + " bytes");
 		System.out.println("Record Size: " + _Mm.recordSize + " bytes");
+		System.out.println("Number of records per block: " + _Mm.recPerBlock);
+		System.out.println("Number of Blocks: " + _Mm.block);
+		System.out.println("################################");
+		System.out.println("Total number of records: " + _Mm.sumNoOfRecords);
+		System.out.println("Total record size: " + _Mm.sumRecSize);
+		System.out.println("Total block size: " + _Mm.sumBlockSize);
+		System.out.println("################################");
 		System.out.println("Size Used: " + _Mm.sise_used + " bytes");
 		System.out.println("Remaining: " + _Mm.remaining + " Blocks");
 		System.out.println("Allocated: " + _Mm.allocated + " Blocks");
-		System.out.println("Total number of records: " + _Mm.sumNoOfRecords);
-		System.out.println("Number of records per block: " + _Mm.recPerBlock);
-		System.out.println("Number of Blocks: " + _Mm.block);
-		System.out.println("Total record size: " + _Mm.sumRecSize);
-		System.out.println("Total block size: " + _Mm.sumBlockSize);
 
-		System.out.println("The Size of database: " + _Mm.sumRecSize + (_Mm.ind_N * _Mm.block_size));
+
+
+
+
 		
 		System.out.println("################ DATABASE INFO END ################");
 	}
